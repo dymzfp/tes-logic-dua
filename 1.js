f823332f101baf907c1a7d6bf7aaeb31e87f2ba7
Dimas Febri <dfebri446@gmail.com>
2019-07-31 10:25:28 +0700

    fix no 3

--- 1.js
+++ 1.js
@@ -1,68 +1,37 @@
-function arrgh(kata) {
+function saham(sah) {
   
-  let dic = ["hot", "dot", "dog", "lot", "log"] 
-  let abjad = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  var kata = kata.split(" ")
-  let strAngka = []
-  let kataAngka = []
-  let dicAngka = []
   
-  for(let i = 0; i < dic.length; i++) {
-    for(let x = 0; x < dic[i].length; x++) {
-      for(let c = 0; c < abjad.length; c++) {    
-          if(abjad[c] == dic[i][x]) {
-            strAngka.push(c)
-          }
-       }
-    }
-    dicAngka.push(strAngka)
-    strAngka = []
-  }
+  let selisih = []
+  let sel, maxProfit
+  let error = 0
   
-  for(let i = 0; i < kata.length; i++) {
-    for(let x = 0; x < kata[i].length; x++) {
-      for(let c = 0; c < abjad.length; c++) {    
-          if(abjad[c] == kata[i][x]) {
-            strAngka.push(c)
-          }
-       }
-    }
-    
-    kataAngka.push(strAngka)
-    strAngka = []
-  }
-  
-  console.log(kataAngka)
-  //console.log(dicAngka)
-  
-  let huruf1 = kataAngka[0]
-  let selisih = [], sel = []
-  
-  //dicAngka.push(kataAngka[1])
-  
-  console.log(dicAngka)
-  
-  for(let y = 0; y < dicAngka.length; y++) {
-    if(dicAngka[y].length = huruf1.length) {
-      for(let u = 0; u < dicAngka[y].length; u++) {
-        if(huruf1[u] > dicAngka[y][u]) {
-          sel.push(huruf1[u]-dicAngka[y][u])
-        }
-        else {
-          sel.push(dicAngka[y][u]-huruf1[u])
-        }
+    for(let i = 0; i < sah.length; i++) {
+      
+      if(sah[i] > sah[i+1]) {
+        error += 1
       }
       
-      selisih.push([dicAngka[y], sel])
-      sel = []
-    }
+      for(let x = i+1; x < sah.length; x++) {
+      
+          if(sah[i] <= sah[x]) {
+            sel = sah[x] - sah[i]
+            selisih.push(sel)
+          }
+      
+      }
     
-  }
+    }
   
-  
-  
-  console.log(selisih[0])
+    if(error == sah.length - 1) {
+      console.log("Tidak dapat memebeli saham")
+    }
+    else {
+      maxProfit = Math.max(...selisih)
+      console.log(maxProfit)
+    }
   
 }
 
-arrgh("hit bot")
+let hargaSaham = [100, 90, 80, 75, 65, 7, 7]
+saham(hargaSaham)
+
